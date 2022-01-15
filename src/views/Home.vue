3112c7e590aad37937364b6dc5233345da0317eb
Yılmaz Daşkafa <ydaskafa@gmail.com>
2022-01-15 23:31:37 +0300

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dignissimos distinctio eaque eligendi fugiat fugit in, incidunt laudantium, nostrum officia pariatur quam rerum, sapiente sequi ut. A accusantium alias at aut autem consequuntur culpa cupiditate dolores ea earum facere, facilis fugit harum id illum laborum nulla officiis qui quibusdam quod recusandae rem reprehenderit tenetur voluptas voluptate. Ad dolores id iusto neque praesentium, quam sed tenetur. Ab dolor eum illo itaque nihil odio, omnis quia quis, reiciendis, sit soluta ut voluptate? Accusantium ad aliquam beatae, dicta ducimus eligendi est, id inventore molestiae mollitia nisi nostrum odit porro possimus quae reiciendis saepe.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dignissimos distinctio eaque eligendi fugiat fugit in, incidunt laudantium, nostrum officia pariatur quam rerum, sapiente sequi ut. A accusantium alias at aut autem consequuntur culpa cupiditate dolores ea earum facere, facilis fugit harum id illum laborum nulla officiis qui quibusdam quod recusandae rem reprehenderit tenetur voluptas voluptate. Ad dolores id iusto neque praesentium, quam sed tenetur. Ab dolor eum illo itaque nihil odio, omnis quia quis, reiciendis, sit soluta ut voluptate? Accusantium ad aliquam beatae, dicta ducimus eligendi est, id inventore molestiae mollitia nisi nostrum odit porro possimus quae reiciendis saepe.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
